19845bc6da7dad0999275901d8cc2e317b086bcc
nikodem redmann <nikodem.redmann@wp.pl>
2023-03-28 23:45:12 +0200

    nmg cos tam dziala

--- src/config.php
+++ src/config.php
@@ -1,6 +1,6 @@
 <?php
 require_once('./../vendor/autoload.php');
-$db = new mysqli("localhost", "root", "", "meme");
+$db = new mysqli("localhost", "root", "", "cms");
 require("Post.class.php");
 require("User.class.php");
 //loader to taki pomocnik do ładowania szablonów
